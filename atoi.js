2fd7a7a35f45b013415cec9df3a93dc5a6fee0c8
iamgabrielsoft <iamgabrielsoft@gmail.com>
2023-04-12 13:03:01 +0100

    solved

--- atoi.js
+++ atoi.js
@@ -3,15 +3,26 @@
  * since we know that the -2^31 = -2147483648 and 2^31 - 1 = 2147483647
  */
 function atoi(s) {
-    function change(val) {
-        if(val > 2147483647) return 2147483647; 
-        if (val < -2147483648) return -2147483648;
-
-
-        console.log(val); 
-        return val; //this could still work val || 0
+    if(typeof s === "string"){
+        return change(
+        +s
+            .trim()
+            .match(/^(^[\-\+]|[0-9])/g)
+            .join("")
+        );
     }
-
-    return change(+s.trim().match(/^(^[\-\+]|[0-9])/g).join(""))
+    
+   console.log('parameter passed is not a string...')
 }
 
+
+
+function change(val) {
+    if (val > 2147483647) return 2147483647;
+    if (val < -2147483648) return -2147483648;
+
+    console.log(val);
+    return val; //this could still work val || 0
+}
+
+atoi("555")
