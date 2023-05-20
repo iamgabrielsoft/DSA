24c2c54f9801793f24aef4d72c70d0898ebee763
iamgabrielsoft <iamgabrielsoft@gmail.com>
2023-05-20 05:42:43 +0100

    all tests passed on leetcode

--- nextPermutation.js
+++ nextPermutation.js
@@ -1,42 +1,41 @@
 
 //[2,1,3] [1,2,3]
 var nextPermutation = (nums) => {
-    let i = nums.length - 2; 
+  const swapping = (i, j) => {
+    [nums[i], nums[j]] = [nums[j], nums[i]];
+  };
 
-    while(i>=0 && nums[i]>= nums[i + 1]) i-- 
-    {
-        console.log(i); 
-        if(i>0){
-            
-            let j = nums.length -1; 
-            console.log(`i: ${i}, j:${j}`); 
-            while(nums[i] >= nums[j]) j--
-            {
-                swap(nums, i, j); 
-            }
-        }
+  const reversing = (left) => {
+    let right = nums.length - 1;
+
+    console.log(right)
+
+    while (left < right) {
+      if (nums[left] < nums[right]) {
+        swapping(left, right);
+        left++;
+        right--;
+      }
     }
+  };
 
-
-   //reverse(arr, i, arr.length - 1); 
-   reverse(nums, i + 1, nums.length - 1)
-
-   function swap(arr, i, j){
-    let temp = arr[i]; 
-    arr[i] = arr[j]; 
-    arr[j] = temp; 
-   }
-
-
-   function reverse(arr, i, j){
-    while(i < j){
-        swap(arr, i++, j--); 
+  for (let i = nums.length - 2; i >= 0; i--) {
+    console.log('i:', i);
+    for (let j = nums.length - 1; j > i; j--) {
+      // console.log(`i: ${i}`, `j: ${j}`);
+      if (nums[i] < nums[j]) {
+        //we can swap the values
+        swapping(i, j);
+        //reverse it to several permutation
+        reversing(i + 1);
+        return;
+      }
     }
-   }
+  }
 
 
-   console.log(nums); 
-   return nums; 
-}
+  reversing(0); //revert to its initial state [1, 2, 3]
 
-nextPermutation([1, 2, 3]);
+  
+  console.log(nums);
+};
