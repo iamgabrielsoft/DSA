f02dd5b42b8cbe88434979aa04b54723fa0c97d5
iamgabrielsoft <iamgabrielsoft@gmail.com>
2023-05-31 13:42:35 +0100

    third changes

--- longestValidParenthesis.js
+++ longestValidParenthesis.js
@@ -83,3 +83,28 @@ var longestValidParenthesis = (s) => {
 
     return max; 
 }
+
+
+/** Third Method
+ * @param {string} s
+ * @return {number}
+ */
+var longestValidParentheses = function (s) {
+  let stack = [-1];
+  let maxLen = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      stack.push(i);
+    } else {
+      stack.pop();
+      if (stack.length > 0) {
+        maxLen = Math.max(maxLen, i - stack[stack.length - 1]);
+      } else {
+        stack.push(i);
+      }
+    }
+  }
+
+  return maxLen;
+};
