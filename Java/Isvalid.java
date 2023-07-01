5529a01423e790331b5564261abc414e1e4d1497
iamgabrielsoft <iamgabrielsoft@gmail.com>
2023-07-01 14:20:02 +0100

    java solution for isvalid problem

---
+++ Java/Isvalid.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+class IsValid {
+    public boolean solution(String s) {
+        Stack<Character> stack = new Stack<>(); 
+
+        for(char ch: s.toCharArray()) {
+            switch(ch){
+                case '(':
+                case '{':
+                case '[':
+                    stack.push(ch); 
+                    break; 
+                case ')':
+                    if(stack.isEmpty() || stack.pop() != '(') {
+                        return false; 
+                    }
+
+                    break; 
+
+                case '}':
+                    if(stack.isEmpty() || stack.pop() != '{') {
+                        return false; 
+                    }
+
+                    break; 
+
+
+                case ']': 
+                    if(stack.isEmpty() || stack.pop() != '[') {
+                        return false; 
+                    }
+
+                    break; 
+            }
+        }
+
+        return stack.isEmpty(); 
+    }
+}
